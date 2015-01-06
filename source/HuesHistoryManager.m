bd7f934fdf4c8ba14b3078f6defe4bc1e9849424
Zach Waugh <zwaugh@gmail.com>
2015-01-05 21:04:23 -0500

    Use dispatch_once singleton

--- source/HuesHistoryManager.m
+++ source/HuesHistoryManager.m
@@ -29,8 +29,6 @@
 
 #define HUES_MAX_HISTORY_SIZE 25
 
-static HuesHistoryManager *sharedHistoryManager = nil;
-
 // Private methods
 @interface HuesHistoryManager ()
 
@@ -38,106 +36,65 @@ static HuesHistoryManager *sharedHistoryManager = nil;
 
 @end
 
-
 @implementation HuesHistoryManager
 
 @synthesize history, menu;
 
-- (id)init
-{
-  if ((self = [super init]))
-  {
-    self.history = [NSMutableArray arrayWithCapacity:HUES_MAX_HISTORY_SIZE];
-  }
-  
-  return self;
+- (id)init {
+    if ((self = [super init])) {
+        self.history = [NSMutableArray arrayWithCapacity:HUES_MAX_HISTORY_SIZE];
+    }
+    
+    return self;
 }
 
-
-- (void)dealloc
-{
-  self.history = nil;
-  [super dealloc];
+- (void)dealloc {
+    self.history = nil;
+    [super dealloc];
 }
-
-
-- (void)addColor:(NSColor *)color
-{
-  // Don't add if same as last color
-  if ([self.history count] > 0 && [[color hues_hex] isEqualToString:[[self.history objectAtIndex:0] hues_hex]]) return;
- 
-  // Ensure doesn't go past max history size
-  if ([self.history count] == HUES_MAX_HISTORY_SIZE)
-  {
-    [self.history removeLastObject];
-  }
-  
-  
-  [self.history insertObject:color atIndex:0];
-   
-  
-  if ([self.menu numberOfItems] == HUES_MAX_HISTORY_SIZE)
-  {
-    [self.menu removeItemAtIndex:HUES_MAX_HISTORY_SIZE - 1];
-  }
-  
-  NSMenuItem *item = [[[NSMenuItem alloc] initWithTitle:[color hues_hex] action:@selector(colorChosen:) keyEquivalent:@""] autorelease];
-  [item setImage:[NSImage imageWithColor:color]];
-  [item setTarget:self];
-  [self.menu insertItem:item atIndex:0];
-}
-
-
-- (void)colorChosen:(id)sender
-{
-  NSInteger index = [self.menu indexOfItem:sender];
-  NSColor *color = [self.history objectAtIndex:index];
-  [[NSNotificationCenter defaultCenter] postNotification:[NSNotification notificationWithName:HuesUpdateColorNotification object:color]];
-}
-
 
 #pragma mark -
 #pragma mark Singleton methods
 
-+ (HuesHistoryManager *)sharedManager
-{
-  if (sharedHistoryManager == nil)
-  {
-    sharedHistoryManager = [[super allocWithZone:NULL] init];
++ (HuesHistoryManager *)sharedManager {
+    static HuesHistoryManager *_sharedManager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _sharedManager = [[HuesHistoryManager alloc] init];
+    });
     
-  }
-  
-  return sharedHistoryManager;
+    return _sharedManager;
 }
 
-+ (id)allocWithZone:(NSZone *)zone
-{
-  return [[self sharedManager] retain];
+- (void)addColor:(NSColor *)color {
+    // Don't add if same as last color
+    if ([self.history count] > 0 && [[color hues_hex] isEqualToString:[[self.history objectAtIndex:0] hues_hex]]) return;
+    
+    // Ensure doesn't go past max history size
+    if ([self.history count] == HUES_MAX_HISTORY_SIZE)
+    {
+        [self.history removeLastObject];
+    }
+    
+    
+    [self.history insertObject:color atIndex:0];
+    
+    
+    if ([self.menu numberOfItems] == HUES_MAX_HISTORY_SIZE)
+    {
+        [self.menu removeItemAtIndex:HUES_MAX_HISTORY_SIZE - 1];
+    }
+    
+    NSMenuItem *item = [[[NSMenuItem alloc] initWithTitle:[color hues_hex] action:@selector(colorChosen:) keyEquivalent:@""] autorelease];
+    [item setImage:[NSImage imageWithColor:color]];
+    [item setTarget:self];
+    [self.menu insertItem:item atIndex:0];
 }
 
-- (id)copyWithZone:(NSZone *)zone
-{
-  return self;
-}
-
-- (id)retain
-{
-  return self;
-}
-
-- (NSUInteger)retainCount
-{
-  return NSUIntegerMax;  //denotes an object that cannot be released
-}
-
-- (void)release
-{
-  //do nothing
-}
-
-- (id)autorelease
-{
-  return self;
+- (void)colorChosen:(id)sender {
+    NSInteger index = [self.menu indexOfItem:sender];
+    NSColor *color = [self.history objectAtIndex:index];
+    [[NSNotificationCenter defaultCenter] postNotification:[NSNotification notificationWithName:HuesUpdateColorNotification object:color]];
 }
 
 @end
